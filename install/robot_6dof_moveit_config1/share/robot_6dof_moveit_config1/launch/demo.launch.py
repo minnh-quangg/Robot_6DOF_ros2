eb4919c9742f67398d5ca57584c22f98ed7bca9f
Dang Minh Hoang <dangminhoang6@gmail.com>
2025-12-12 10:13:11 +0700

    git ignore

--- install/robot_6dof_moveit_config1/share/robot_6dof_moveit_config1/launch/demo.launch.py
+++ install/robot_6dof_moveit_config1/share/robot_6dof_moveit_config1/launch/demo.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("robot_6dof", package_name="robot_6dof_moveit_config2").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("robot_6dof", package_name="robot_6dof_moveit_config1").to_moveit_configs()
     return generate_demo_launch(moveit_config)
